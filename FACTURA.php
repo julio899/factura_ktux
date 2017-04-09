6074eed202fa36db65bf5e7c15a34b2d2697995c
Julio Vinachi <julio899@gmail.com>
2017-04-08 22:57:49 -0400

    Update FACTURA.php

--- FACTURA.php
+++ FACTURA.php
@@ -2,16 +2,17 @@
 require('class_factura.php');
 
 $productos[]=array(	'cantidad' => 100,
-					'descripcion' => strtoupper("Estuches usos mixtos"),
-					'precioUnitario' =>60.00
-			);
+			'descripcion' => strtoupper("Estuches usos mixtos"),
+			'precioUnitario' =>60.00
+		);
+
 $nueva_factura = array(
-							'razon'			=>'Julio Vinachi',
-							'rif'			=>'V-18266159',
-							'direccion1'	=>'sector santa rita, urb. el valle',
-							'direccion2'	=>'Maracay, Edo. Aragua',
-							'fecha'			=>'19/11/2015',
-							'productos'		=>$productos
-						);
+			'razon'		=>'Julio Vinachi',
+			'rif'		=>'V-18266159',
+			'direccion1'	=>'sector santa rita, urb. el valle',
+			'direccion2'	=>'Maracay, Edo. Aragua',
+			'fecha'		=>'19/11/2015',
+			'productos'	=>$productos
+		      );
 
 new Factura($nueva_factura);
