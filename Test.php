8b3996a96ff6e7e766be09b490d1b00abef35964
Julio Vinachi <julio899@gmail.com>
2018-03-11 20:56:58 -0400

    actualizando tester

--- Test.php
+++ Test.php
@@ -1,19 +1,20 @@
-<?php use ktux\models\FacturaModel as FacturaModel;
-require_once('vendor/autoload.php');
+ <?php require_once('vendor/autoload.php');
+use ktux\models\FacturaModel as FacturaKtux;
 /* Probador */
 $productos[]=array(	'cantidad' => 100,
 			'descripcion' => strtoupper("Estuches usos mixtos"),
-			'precioUnitario' =>60.00
+			'precioUnitario' =>1600.00
 		);
 
 $nueva_factura = array(
-			'razon'		=>'Julio Vinachi',
-			'rif'		=>'V-18266159',
-			'direccion1'	=>'sector santa rita, urb. el valle',
+			'razon'		=>'Probando Tester',
+			'rif'		=>'V-12345678',
+			'direccion1'	=>'sector las delicias, urb. canta rana',
 			'direccion2'	=>'Maracay, Edo. Aragua',
 			'fecha'		=>'19/11/2015',
 			'productos'	=>$productos
 		      );
 
-$t = new FacturaModel($nueva_factura);
+$f = new FacturaKtux($nueva_factura);
+ 
  ?>
