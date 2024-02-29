9851128beb7431b0b17f53b778bcdeacda59715a
Julio Vinachi <julio899@gmail.com>
2024-02-29 13:28:32 -0400

    deb 2024

--- FACTURA.php
+++ FACTURA.php
@@ -3,16 +3,16 @@ require('class_factura.php');
 
 $productos[]=array(	'cantidad' => 1,
 					'descripcion' => strtoupper("Servicios de desarrollo de software"),
-					'precioUnitario' =>1250.00
+					'precioUnitario' =>1000.00
 			);
 $nueva_factura = array(
 							'razon'			=>'Neogaleno S A de C V',
 							'rif'			=>'NEO220704EP6',
 							'direccion1'	=>'Lago Alberto 320 2512, Anahuac I Secc.',
 							'direccion2'	=>'Miguel Hidalgo, CP 11320. CDMX.',
-							'fecha'			=>'21/12/2023', // '29/09/2023',
+							'fecha'			=>'29/02/2024', // '29/09/2023',
 							'productos'		=>$productos,
-							'nro'			=> '130',
+							'nro'			=> '133',
 						);
 
 new Factura($nueva_factura);
