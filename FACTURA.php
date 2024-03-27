eddf4e17bf5aea8fff72844b08587cce67201995
Julio Vinachi <julio899@gmail.com>
2024-03-27 15:00:14 -0400

    marz 2024

--- FACTURA.php
+++ FACTURA.php
@@ -10,9 +10,9 @@ $nueva_factura = array(
 							'rif'			=>'NEO220704EP6',
 							'direccion1'	=>'Lago Alberto 320 2512, Anahuac I Secc.',
 							'direccion2'	=>'Miguel Hidalgo, CP 11320. CDMX.',
-							'fecha'			=>'29/02/2024', // '29/09/2023',
+							'fecha'			=>'27/03/2024', // '29/09/2023',
 							'productos'		=>$productos,
-							'nro'			=> '133',
+							'nro'			=> '134',
 						);
 
 new Factura($nueva_factura);
