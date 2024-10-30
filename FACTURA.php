98e5ffe667565d866af303f1532db1a9fca2d39f
Julio Vinachi <julio899@gmail.com>
2024-10-30 14:52:24 -0400

    control 141 docker-composer up -d

--- FACTURA.php
+++ FACTURA.php
@@ -3,16 +3,17 @@ require('class_factura.php');
 
 $productos[]=array(	'cantidad' => 1,
 					'descripcion' => strtoupper("Servicios de desarrollo de software"),
-					'precioUnitario' =>1050.00
+					'precioUnitario' => 950
+					// 1050.00
 			);
 $nueva_factura = array(
 							'razon'			=>'Neogaleno S A de C V',
 							'rif'			=>'NEO220704EP6',
 							'direccion1'	=>'Lago Alberto 320 2512, Anahuac I Secc.',
 							'direccion2'	=>'Miguel Hidalgo, CP 11320. CDMX.',
-							'fecha'			=>'31/08/2024',
+							'fecha'			=>'30/10/2024',
 							'productos'		=>$productos,
-							'nro'			=> '139',
+							'nro'			=> '141',
 						);
 
 new Factura($nueva_factura);
